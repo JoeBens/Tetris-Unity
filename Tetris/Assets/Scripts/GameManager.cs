f6325b83488472a6e90c1c88171701e812d3accd
Youcef Benslimane <43045520+JoeBens@users.noreply.github.com>
2019-07-14 17:16:34 +0200

    Tetris prototype

---
+++ Tetris/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] tetrominoes;
+    
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+        SpawnTetromino();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void SpawnTetromino()
+    {
+        Instantiate(tetrominoes[Random.Range(0, tetrominoes.Length)], transform.position, Quaternion.identity);
+
+
+    }
+}
---
+++ Tetris/Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour {
+
+
+    public Vector3 rotationPoint;
+
+
+    private float previousTime;
+    public float fallTime = 0.8f;
+
+    public static int height = 20;
+    public static int width = 10;
+
+    private static Transform[,] grid = new Transform[width, height];
+
+
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!isValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!isValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+            if (!isValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            }
+
+        }
+
+        if (Time.time -previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime /10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!isValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckLines();
+                this.enabled = false;
+                FindObjectOfType<GameManager>().SpawnTetromino();
+
+            }
+            previousTime = Time.time;
+        }
+
+	}
+
+    void CheckLines()
+    {
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (isLineFull(i))
+            {
+                DestroyBlockLine(i);
+                MoveDown(i);
+            }
+        }
+    }
+
+    bool isLineFull(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+
+        return true;
+    }
+
+    void DestroyBlockLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void MoveDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+        }
+    }
+
+
+
+    bool isValidMove()
+    {
+        foreach (Transform child in transform)
+        {
+            int roundedX = Mathf.RoundToInt(child.transform.position.x);
+            int roundedY = Mathf.RoundToInt(child.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+                
+
+            if (grid[roundedX, roundedY] != null)
+            {
+                Debug.Log("Taken");
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+
+}
